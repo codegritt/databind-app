857243ce4c25ecfb19dfb5cd591f30d99d05960d
Gokul <gokulchandan18@gmail.com>
2021-12-05 17:04:05 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'databind-app';
+  loadedfeature = 'recipe';
+  onNavigate(feature: string){
+    this.loadedfeature= feature;
+  }
 }
+
+
